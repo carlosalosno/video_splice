5df64c0fa79a1f556ac2c8eeb853f10cb4ea30a6
carlosalosno <carlos.alonso@techex.es>
2019-10-02 17:06:01 +0200

    añadido ficcheros castellano

---
+++ languages/es.php
@@ -0,0 +1,28 @@
+<?php
+# English
+# Language File for the Video Splice Plugin
+# -------
+#
+#
+$lang["action-cut"]="Cortar";
+$lang["error-from_time_after_to_time"]="Error: 'desde' tiene que estar antes que 'hasta'.";
+$lang["from-time"]="Desde";
+$lang["to-time"]="Hasta";
+$lang["hh"]="HH";
+$lang["mm"]="MM";
+$lang["ss"]="SS";
+
+$lang["videospliceconfiguration"]="Configuracón del Compactador";
+$lang["specify_resource_type"]="Especifica para que tipo de recurso se habilitará el cortador y compactador.";
+$lang["video_resource_type"]="Tipo de recurso de vídeo";
+$lang['cut_original']="Cortar desde calidad original";
+$lang["specify_parent_field"]="Especifica que campo será usado para volcar la información de cort/compactado.";
+$lang["parent_resource_field"]="Campo de Información del Recurso Padre";
+
+$lang["splice"]="Compactar";
+$lang["intro-splice"]="Une varios recursos de vídeo para formar un recurso de vídeo compactado..";
+$lang["drag_and_drop_to_rearrange"]="Arrastrar y soltar para reordenar los clips de vídeo.";
+$lang["action-splice"]="Compactar";
+$lang["error-no-ffmpegpreviewfile"]="Error: Video %resourceid no tiene un tipo de %filetype asociado como recurso principal. ¿Es el resultado de un corte?"; # %resourceid, %filetype will be replaced
+$lang["cropped_from_resource"]="(cortado de %resourceinfo)"; # %resourceinfo will be replaced
+$lang["merged_from_resources"]="Compactado de %resources"; # %resources will be replaced
